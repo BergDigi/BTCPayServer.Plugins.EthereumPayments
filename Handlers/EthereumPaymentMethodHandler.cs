8ea37893376f19e07e76d892e64c1643ab01d064
Berggold digital <mail@berggold.digital>
2026-02-27 09:05:54 +0100

    Create EthereumPaymentMethodHandler.cs

---
+++ Handlers/EthereumPaymentMethodHandler.cs
@@ -0,0 +1,19 @@
+using BTCPayServer.Payments;
+
+namespace BTCPayServer.Plugins.EthereumPayments.Handlers;
+
+public class EthereumPaymentMethodHandler
+{
+    // TODO: Implement IPaymentMethodHandler interface
+    // Dies erfordert tiefere Integration mit BTCPayServer's Payment System
+}
+
+public class dEUROPaymentMethodHandler
+{
+    // TODO: ERC-20 Token Handler für dEURO
+}
+
+public class USDTPaymentMethodHandler
+{
+    // TODO: ERC-20 Token Handler für USDT
+}
